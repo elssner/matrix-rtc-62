ea7dcd4d100425cba8eb5b5378854d369f6e46d8
GTA Calliope <73078489+elssner@users.noreply.github.com>
2024-04-04 20:47:58 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,9 @@
 function display64 (text: string) {
-    matrix.line(49, 0, 127, 47)
-    matrix.line(49, 0, 127, 0)
-    matrix.line(49, 47, 127, 47)
+    matrix.line(50, 0, 127, 0)
+    matrix.line(50, 47, 127, 47)
     matrix.line(127, 47, 127, 0)
-    matrix.line(49, 0, 49, 47)
-    matrix.writeDigitImageArray(text, 53, 39, 7, -4, matrix.eTransparent.t)
+    matrix.line(50, 0, 50, 47)
+    matrix.writeDigitImageArray(text, 54, 39, 7, -4, matrix.eTransparent.t)
 }
 function display128 (text: string) {
     matrix.clearMatrix()
@@ -12,8 +11,13 @@ function display128 (text: string) {
     matrix.rasterCircle(64, 96, 31)
     matrix.line(64, 64, 127, 127)
     matrix.writeDigitImageArray(text, 0, 0, 8, 0, matrix.eTransparent.u)
-    matrix.writeDigitImageArray(text, 64, 24, -10, -2, matrix.eTransparent.t, matrix.oled_eFaktor(matrix.eFaktor.f2))
+    matrix.writeDigitImageArray(text, 115, 10, -10, -2, matrix.eTransparent.t, matrix.oled_eFaktor(matrix.eFaktor.f2))
     matrix.writeDigitImageArray(text, 0, 64, 25, -3, matrix.eTransparent.t, matrix.oled_eFaktor(matrix.eFaktor.f5), matrix.oled_eFaktor(matrix.eFaktor.f8))
+    matrix.writeImage(matrix.imageDrehen(matrix.digitImage(rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Stunde), rtcpcf85063tp.eFormat.zehner)), matrix.eZeichenDrehen.rechts), 0, 22, matrix.eTransparent.t, matrix.oled_eFaktor(matrix.eFaktor.f3))
+    matrix.writeImage(matrix.imageDrehen(matrix.digitImage(rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Stunde), rtcpcf85063tp.eFormat.einer)), matrix.eZeichenDrehen.links), 25, 22, matrix.eTransparent.t, matrix.oled_eFaktor(matrix.eFaktor.f3))
+    matrix.writeDigitImageArray(":", 50, 22, 8, 0, matrix.eTransparent.t, matrix.oled_eFaktor(matrix.eFaktor.f4))
+    matrix.writeImage(matrix.imageDrehen(matrix.digitImage(rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Minute), rtcpcf85063tp.eFormat.zehner)), matrix.eZeichenDrehen.halb), 60, 22, matrix.eTransparent.t, matrix.oled_eFaktor(matrix.eFaktor.f3))
+    matrix.writeImage(matrix.imageDrehen(matrix.digitImage(rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Minute), rtcpcf85063tp.eFormat.einer)), matrix.eZeichenDrehen.xspiegeln), 85, 22, matrix.eTransparent.t, matrix.oled_eFaktor(matrix.eFaktor.f3))
     matrix.writeDisplay()
 }
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
@@ -29,7 +33,7 @@ matrix.writeDisplay(0, 7, matrix.eI2C.I2C_x3D)
 rtcpcf85063tp.beimStart(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51))
 loops.everyInterval(1000, function () {
     rtcpcf85063tp.readDateTime(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51))
-    if (rtcpcf85063tp.isChanged(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Sekunde))) {
+    if (rtcpcf85063tp.isChanged(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Minute))) {
         matrix.clearMatrix(0, 5)
         matrix.writeClock_radius24(24, 23, rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Stunde), rtcpcf85063tp.eFormat.DEC), rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Minute), rtcpcf85063tp.eFormat.DEC))
         display64(rtcpcf85063tp.getDate(rtcpcf85063tp.ePart.ohne, rtcpcf85063tp.ePart.mit))
