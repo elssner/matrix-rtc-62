53e3a52ec7a2c52edd8318e7ad776040e37eed21
GTA Calliope <73078489+elssner@users.noreply.github.com>
2024-04-14 14:15:26 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,8 @@ function display64 (text: string) {
     matrix.line(50, 47, 127, 47)
     matrix.line(127, 47, 127, 0)
     matrix.line(50, 0, 50, 47)
-    matrix.writeDigitImageArray(text, 54, 39, 7, -4, matrix.eTransparent.t)
+    matrix.imageArrayDigit(matrix.matrix_text(text))
+    matrix.writeImageArray(matrix.matrix_Images(), 54, 39, 7, -4, matrix.eTransparent.t)
 }
 function display128 (text: string) {
     matrix.clearMatrix()
@@ -12,13 +13,13 @@ function display128 (text: string) {
     matrix.rasterCircle(64, 96, 31)
     matrix.line(64, 64, 127, 127)
     matrix.writeDigits(0, 0, 4, matrix.matrix_text(text), matrix.eAlign.links, 8, 0, matrix.eTransparent.u)
-    matrix.writeDigitImageArray(text, 115, 10, -10, -2, matrix.eTransparent.t, matrix.oled_eFaktor(matrix.eFaktor.f2))
-    matrix.writeDigitImageArray(text, 0, 64, 25, -3, matrix.eTransparent.t, matrix.oled_eFaktor(matrix.eFaktor.f5), matrix.oled_eFaktor(matrix.eFaktor.f8))
-    matrix.writeImage(matrix.imageDrehen(matrix.getImage(0), matrix.eZeichenDrehen.rechts), 0, 22, matrix.eTransparent.t, matrix.oled_eFaktor(matrix.eFaktor.f3))
-    matrix.writeImage(matrix.imageDrehen(matrix.getImage(1), matrix.eZeichenDrehen.links), 25, 22, matrix.eTransparent.t, matrix.oled_eFaktor(matrix.eFaktor.f3))
-    matrix.writeDigitImageArray(":", 50, 22, 8, 0, matrix.eTransparent.t, matrix.oled_eFaktor(matrix.eFaktor.f4))
-    matrix.writeImage(matrix.imageDrehen(matrix.getImage(3), matrix.eZeichenDrehen.halb), 60, 22, matrix.eTransparent.t, matrix.oled_eFaktor(matrix.eFaktor.f3))
-    matrix.writeImage(matrix.imageDrehen(matrix.getImage(4), matrix.eZeichenDrehen.xspiegeln), 85, 22, matrix.eTransparent.t, matrix.oled_eFaktor(matrix.eFaktor.f3))
+    matrix.writeImageArray(matrix.matrix_Images(), 115, 10, -10, -2, matrix.eTransparent.t, matrix.matrix_eFaktor(matrix.eFaktor.f2))
+    matrix.writeImageArray(matrix.matrix_Images(), 0, 64, 25, -3, matrix.eTransparent.t, matrix.matrix_eFaktor(matrix.eFaktor.f5), matrix.matrix_eFaktor(matrix.eFaktor.f8))
+    matrix.writeImage(matrix.imageDrehen(matrix.getImage(0), matrix.eZeichenDrehen.rechts), 0, 22, matrix.eTransparent.t, matrix.matrix_eFaktor(matrix.eFaktor.f3))
+    matrix.writeImage(matrix.imageDrehen(matrix.getImage(1), matrix.eZeichenDrehen.links), 25, 22, matrix.eTransparent.t, matrix.matrix_eFaktor(matrix.eFaktor.f3))
+    matrix.writeImage(matrix.imageDrehen(matrix.getImage(2), matrix.eZeichenDrehen.nicht), 50, 22, matrix.eTransparent.t, matrix.matrix_eFaktor(matrix.eFaktor.f4))
+    matrix.writeImage(matrix.imageDrehen(matrix.getImage(3), matrix.eZeichenDrehen.halb), 60, 22, matrix.eTransparent.t, matrix.matrix_eFaktor(matrix.eFaktor.f3))
+    matrix.writeImage(matrix.imageDrehen(matrix.getImage(4), matrix.eZeichenDrehen.xspiegeln), 85, 22, matrix.eTransparent.t, matrix.matrix_eFaktor(matrix.eFaktor.f3))
     matrix.displayMatrix()
 }
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
@@ -42,6 +43,6 @@ loops.everyInterval(1000, function () {
         display128(rtcpcf85063tp.getTime(rtcpcf85063tp.ePart.ohne))
     }
     matrix.clearMatrix(6, 7)
-    matrix.writeDigitImageArray(rtcpcf85063tp.getTime(rtcpcf85063tp.ePart.mit), 1, 49, 16, 0, matrix.eTransparent.u, matrix.oled_eFaktor(matrix.eFaktor.f2))
+    matrix.writeDigits(6, 0, 15, rtcpcf85063tp.getTime(rtcpcf85063tp.ePart.mit), matrix.eAlign.links, 16, 0, matrix.eTransparent.u, matrix.matrix_eFaktor(matrix.eFaktor.f2))
     matrix.writeDisplay(6, 7, matrix.eI2C.I2C_x3D)
 })
